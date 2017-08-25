9fe163799c79e82b6fdaf68dc84157e016388e3f
suyingda <syq1023032120@qq.com>
2017-08-25 13:04:22 +0800

    初始化

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { JokesComponent } from './jokes/jokes.component'
+
+export const appRoutes = [
+   /*  {
+        path: '',
+        redirecTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'jokes',
+        component: JokesComponent
+    },
+    {
+        path: '*',
+        component: HomeComponent
+    }, */
+]
